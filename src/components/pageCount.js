4dd938ffa140d0fd87e2477403644ce99b2cf6ab
nerte <grimza@nerteui-MacBookPro.local>
2025-01-15 23:01:44 +0900

    [Design] 정렬버튼, 검색창과 검색버튼 스타일

--- src/components/pageCount.js
+++ src/components/pageCount.js
@@ -1,22 +1,51 @@
+import styled from "styled-components";
+
+const PageBtn = styled.div`
+  display: flex;
+  gap: 4px;
+  margin: 43px auto 58px;
+  text-align: center;
+  width: 304px;
+`;
+const Btn = styled.button`
+  width: 40px;
+  height: 40px;
+  border-radius: 40px;
+  border: 1px solid #e5e7eb;
+  opacity: 0px;
+  color: #6b7280;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 26px;
+  background-color: #ffffff;
+`;
+
 function PageCount({ onClick }) {
   return (
-    <div>
-      <button value="1" onClick={onClick}>
+    <PageBtn>
+      <Btn value="previous" onClick={onClick}>
+        전
+      </Btn>
+      <Btn value="1" onClick={onClick}>
         1
-      </button>
-      <button value="2" onClick={onClick}>
+      </Btn>
+      <Btn value="2" onClick={onClick}>
         2
-      </button>
-      <button value="3" onClick={onClick}>
+      </Btn>
+      <Btn value="3" onClick={onClick}>
         3
-      </button>
-      <button value="4" onClick={onClick}>
+      </Btn>
+      <Btn value="4" onClick={onClick}>
         4
-      </button>
-      <button value="5" onClick={onClick}>
+      </Btn>
+      <Btn value="5" onClick={onClick}>
         5
-      </button>
-    </div>
+      </Btn>
+      <Btn value="next" onClick={onClick}>
+        후
+      </Btn>
+    </PageBtn>
   );
 }
 export default PageCount;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,8 +3,47 @@ import ItemsList from "../components/ItemsList";
 import { getProducts, bestProducts } from "../api";
 import { Link } from "react-router-dom";
 import PageCount from "../components/pageCount";
+import styled from "styled-components";
 //
+const InputDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const SearchBtn = styled.button`
+  height: 42px;
+  padding: 12px 23px;
+  border-radius: 8px;
+  color: #f3f4f6;
+  background-color: #3692ff;
+  border: none;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 26px;
+`;
+const InputForm = styled.input`
+  width: 325px;
+  height: 42px;
+  padding: 9px 20px 9px 16px;
+  border-radius: 12px;
+  border: none;
+  background-color: #f3f4f6;
+  color: #9ca3af;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 26px;
+  text-align: left;
+`;
 
+const SortSelect = styled.select`
+  width: 130px;
+  height: 42px;
+  padding: 12px 20px 12px 20px;
+  border-radius: 12px;
+  border: 1px solid #e5e7eb;
+`;
+//
 function HomePage() {
   const [items, setItems] = useState([]);
   const [order, setOrder] = useState("recent");
@@ -37,21 +76,23 @@ function HomePage() {
         <h3>베스트 상품</h3>
         <ItemsList items={bestItems} />
       </div>
-      <div>
+      <InputDiv>
         <h3> 전체 상품</h3>
         <div>
-          <input type="text"></input>
+          <InputForm
+            type="text"
+            placeholder="검색할 상품을 입력해주세요"
+          ></InputForm>
           <Link to="/additem">
-            <button type="submit">상품 등록하기</button>
+            <SearchBtn type="submit">상품 등록하기</SearchBtn>
           </Link>
+          <SortSelect value={order} onChange={handleChangeSort}>
+            <option value="recent">최신순</option>
+            <option value="favorite">좋아요 순</option>
+          </SortSelect>
         </div>
-        <select value={order} onChange={handleChangeSort}>
-          <option value="recent">최신순</option>
-          <option value="favorite">좋아요 순</option>
-        </select>
-
-        <ItemsList items={items} />
-      </div>
+      </InputDiv>
+      <ItemsList items={items} />
       <PageCount onClick={handleClickPageChange} />
     </div>
   );
