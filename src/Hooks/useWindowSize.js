65157fab33a1939bc55783b7635e3d738587d481
nerte <grimza@nerteui-MacBookPro.local>
2025-02-14 07:29:39 +0900

    [Refactor] 코드 리뷰에 따른 windowSize 리팩토링

--- src/Hooks/useWindowSize.js
+++ src/Hooks/useWindowSize.js
@@ -2,24 +2,24 @@ import { useState, useEffect } from "react";
 import throttle from "lodash.throttle";
 export default function useWindowSize() {
   const [deviceType, setDeviceType] = useState("desktop");
-  const handleResize = throttle(() => {
-    if (window.matchMedia("(max-width: 375px)").matches) {
-      setDeviceType("mobile");
-      return;
-    }
-    if (window.matchMedia("(max-width: 768px)").matches) {
-      setDeviceType("tablet");
-      return;
-    }
-    setDeviceType("desktop");
-  }, 2000);
-
+  //
   useEffect(() => {
+    const handleResize = throttle(() => {
+      if (window.matchMedia("(max-width: 375px)").matches) {
+        setDeviceType("mobile");
+        return;
+      }
+      if (window.matchMedia("(max-width: 768px)").matches) {
+        setDeviceType("tablet");
+        return;
+      }
+      setDeviceType("desktop");
+    }, 2000);
     handleResize();
     window.addEventListener("resize", handleResize);
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, [handleResize]);
+  }, []);
   return deviceType;
 }
