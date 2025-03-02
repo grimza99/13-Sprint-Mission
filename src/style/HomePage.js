0caf94e050918e7f1dbce3ae8787c4234bca21f8
nerte <grimza@nerteui-MacBookPro.local>
2025-03-02 16:42:56 +0900

    [Chore] Next 프로젝트로 변경

--- src/style/HomePage.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-import ItemsList from "../components/ItemsList";
-// import HomePage from '../pages/HomePage';
-
-const ListGrid = styled(ItemsList)`
-  display: grid;
-`;
