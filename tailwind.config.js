0c4bb845bc15cf2fc4a411fedd57f3522bff71c1
nerte <grimza@nerteui-MacBookPro.local>
2025-03-08 00:55:11 +0900

    [Chore] npm install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}", // Next.js 페이지 경로
+    "./components/**/*.{js,ts,jsx,tsx}", // 컴포넌트 경로
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
