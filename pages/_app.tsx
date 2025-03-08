9783a4cc12f0f97d62daceca2b7ca396c05683be
nerte <grimza@nerteui-MacBookPro.local>
2025-03-08 12:41:22 +0900

    [Chore] delete layout.style.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import Nav from "../components/Layout/Nav/Nav";
 import type { AppProps } from "next/app";
-import * as S from "../components/Layout/Layout.style";
 import "../styles/global.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
