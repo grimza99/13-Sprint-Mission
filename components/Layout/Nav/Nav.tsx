5cabe04979dcb0847c9dd38e588f679ee027be4d
nerte <grimza@nerteui-MacBookPro.local>
2025-03-08 15:45:24 +0900

    [Refactor] Nav style changed tailwind

--- components/Layout/Nav/Nav.tsx
+++ components/Layout/Nav/Nav.tsx
@@ -2,34 +2,45 @@ import Link from "next/link";
 import pandaLogo from "../../../public/assets/Logo/pandaLogo.svg";
 import textLogo from "../../../public/assets/Logo/textLogo.svg";
 import myLogo from "../../../public/assets/icons/default.profile.icon.svg";
-import * as S from "./Nav.style";
+import Image from "next/image";
 import useWindowSize from "../../../hooks/useWindowSize";
 export default function Nav() {
   const device: string = useWindowSize();
   return (
-    <div>
-      <S.HeaderArea>
-        <S.ContentsWrapper>
-          <S.Contents>
-            <S.NavContents>
-              <S.LogoContainer>
-                {device !== "mobile" && (
-                  <S.PandaImg src={pandaLogo} alt="판다로고" />
-                )}
-                <S.Logo src={textLogo} alt="판다마켓" />
-              </S.LogoContainer>
-
-              <S.NavLinkWrapper>
-                <Link href="/docs">자유게시판</Link>
-                <Link href="/items">중고마켓</Link>
-              </S.NavLinkWrapper>
-            </S.NavContents>
-            <Link href="/mypage">
-              <S.MyLogoButton src={myLogo} alt="프로필 이미지" />
-            </Link>
-          </S.Contents>
-        </S.ContentsWrapper>
-      </S.HeaderArea>
+    <div className="fixed top-0 left-0 w-screen h-[70px] bg-white px-[10px] py-[200px] md:px-[24px] md:py-[10px]  sm:px-[15px] sm:py-[15px] z-50">
+      <div className="flex justify-between w-screen max-w-full ">
+        <div className="flex items-center w-screen gap-8 sm:gap-2">
+          <div className="flex w-auto gap-2">
+            <Image
+              className="w-10 h-10 sm:hidden"
+              src={pandaLogo}
+              alt="판다로고"
+              width={40}
+              height={40}
+            />
+            <Image
+              className="w-[103px] h-[51px] cursor-pointer sm:w-20"
+              src={textLogo}
+              alt="판다마켓"
+              width={103}
+              height={51}
+            />
+          </div>
+          <div className="flex w-auto gap-7 sm:gap-2">
+            <Link href="/docs">자유게시판</Link>
+            <Link href="/items">중고마켓</Link>
+          </div>
+        </div>
+        <Link href="/mypage">
+          <Image
+            className="w-10 h-10"
+            src={myLogo}
+            alt="프로필 이미지"
+            width={40}
+            height={40}
+          />
+        </Link>
+      </div>
     </div>
   );
 }
