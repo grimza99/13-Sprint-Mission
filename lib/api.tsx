9d4c23eee68f955162976b4b883f9c727498037c
nerte <grimza@nerteui-MacBookPro.local>
2025-04-05 21:58:28 +0900

    [Feat] interceptor 로 엑세스 토큰 bearer header 추가

--- lib/api.tsx
+++ lib/api.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosResponse, InternalAxiosRequestConfig } from "axios";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
@@ -6,7 +6,26 @@ const instance = axios.create({
   baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
+    withCredentials: true,
   },
 });
 
 export default instance;
+
+instance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (typeof window !== "undefined" && accessToken) {
+      config.headers.set("Authorization", `Bearer ${accessToken}`);
+    }
+    return config;
+  }
+);
+
+instance.interceptors.response.use((response: AxiosResponse) => {
+  const accessToken = response.data.accessToken;
+  if (typeof window !== "undefined" && accessToken) {
+    localStorage.setItem("accessToken", accessToken);
+  }
+  return response;
+});
